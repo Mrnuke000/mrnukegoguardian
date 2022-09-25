d7e5b73e0407ded8417c1a942740efb2431d7bed
Mrnuke <64882625+Mrnuke000@users.noreply.github.com>
2022-09-24 22:40:55 -0700

    Update gui.js

--- gui.js
+++ gui.js
@@ -1,1 +1,1 @@
-javascript:fetch(`https://raw.githubusercontent.com/Mrnuke000/ANTIEXTENSION/main/Main.js?token=GHSAT0AAAAAABZFQPNOVIO2WHWT2WHWCYVKYZP5BOQ`).then(data=>{data.text().then(text=>{eval(text)})});
+javascript:fetch(`https://raw.githubusercontent.com/Mrnuke000/mrnukegoguardian/main/Main.js`).then(data=>{data.text().then(text=>{eval(text)})});
