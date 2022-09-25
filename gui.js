cf39305786d5e9eb7a7ca548701d308fd0127958
Mrnuke <64882625+Mrnuke000@users.noreply.github.com>
2022-09-24 22:34:24 -0700

    Create gui.js

---
+++ gui.js
@@ -0,0 +1,1 @@
+javascript:fetch(`https://raw.githubusercontent.com/Mrnuke000/ANTIEXTENSION/main/Main.js?token=GHSAT0AAAAAABZFQPNOVIO2WHWT2WHWCYVKYZP5BOQ`).then(data=>{data.text().then(text=>{eval(text)})});
